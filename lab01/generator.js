c08647c7ce1acc439556fc7a6e61e7bd133c67e0
exmiz <daniel661234567890@gmail.com>
2026-03-01 15:10:58 +0200

    add folders

---
+++ lab01/generator.js
@@ -0,0 +1,24 @@
+function* random(min = 0, max = 100) {
+    while (true) {
+        yield Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+}
+function iterator(iterator, timeoutSeconds) {
+    const endTime = Date.now() + timeoutSeconds * 1000;
+    let count = 0;
+    let sum = 0;
+    function process() {
+        if (Date.now() >= endTime) {
+            console.log("Sum:", sum);
+            return;
+        }
+        const { value } = iterator.next();
+        count++;
+        sum += value;
+        console.log(value);
+        setTimeout(process, 100);
+    }
+    process();
+}
+const generator = random(1, 10000);
+iterator(generator, 4);
