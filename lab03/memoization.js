1f3e81bb16a7767e4a7f0a9094de0fe3205fc595
exmiz <daniel661234567890@gmail.com>
2026-03-28 20:09:22 +0200

    lab03.2

--- lab03/memoization.js
+++ lab03/memoization.js
@@ -1,17 +1,78 @@
-function memoization(fn) {
-  const cache = new Map();
+function memoization(fn, options = {}) {
+    const {
+        maxSize = Infinity,
+        strategy = "LRU",
+        ttl = null,
+        customEvict = null
+    } = options;
+    const cache = new Map();
 
-  return function (...args) {
-    const key = JSON.stringify(args);
-
-    if (cache.has(key)) {
-      return cache.get(key);
+    function getKey(args) {
+        return JSON.stringify(args);
     }
-    const result = fn(...args);
-    cache.set(key, result);
+    function evictIfNeeded() {
+        if (cache.size <= maxSize) return;
+        if (strategy === "LRU") {
+            const firstKey = cache.keys().next().value;
+            cache.delete(firstKey);
+        }
 
-    return result;
-  }
+        else if (strategy === "LFU") {
+            let minFreq = Infinity;
+            let keyToDelete = null;
+
+            for (let [key, value] of cache.entries()) {
+                if (value.freq < minFreq) {
+                    minFreq = value.freq;
+                    keyToDelete = key;
+                }
+            }
+
+            if (keyToDelete !== null) {
+                cache.delete(keyToDelete);
+            }
+        }
+
+        else if (strategy === "CUSTOM" && typeof customEvict === "function") {
+            const keyToDelete = customEvict(cache);
+            if (keyToDelete !== undefined) {
+                cache.delete(keyToDelete);
+            }
+        }
+    }
+    return function (...args) {
+        const key = getKey(args);
+        const now = Date.now();
+
+        if (cache.has(key)) {
+            const entry = cache.get(key);
+    
+            if (ttl && now - entry.time > ttl) {
+                cache.delete(key);
+            } else {
+                entry.freq++;
+                entry.time = now;
+
+                if (strategy === "LRU") {
+                    cache.delete(key);
+                    cache.set(key, entry);
+                }
+
+                return entry.value;
+            }
+        }
+        const result = fn(...args);
+
+        cache.set(key, {
+            value: result,
+            freq: 1,
+            time: now
+        });
+
+        evictIfNeeded();
+
+        return result;
+    }
 }
 const add = (a, b) => {
     console.log("first computing");
