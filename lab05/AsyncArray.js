7804c71c6705d779976b3fe3b178ce56a7513768
exmiz <daniel661234567890@gmail.com>
2026-04-26 14:37:42 +0300

    lab05.01

---
+++ lab05/AsyncArray.js
@@ -0,0 +1,54 @@
+function asyncMapCallback(arr, fn, done) {
+  let result = [];
+  let count = 0;
+  arr.forEach((item, i) => {
+    fn(item, (err, res) => {
+      if (err) return done(err);
+
+      result[i] = res;
+      count++;
+
+      if (count === arr.length) {
+        done(null, result);
+      }
+    })
+  })
+}
+
+function asyncMapPromise(arr, fn, signal) {
+  return new Promise((resolve, reject) => {
+    if (signal?.aborted) return reject("Сancel")
+    let result = [];
+    let count = 0;
+
+    arr.forEach((item, i) => {
+      fn(item)
+        .then(res => {
+          if (signal?.aborted) return reject("Сancel");
+
+          result[i] = res;
+          count++;
+
+          if (count === arr.length) {
+            resolve(result);
+          }
+        })
+        .catch(reject);
+    })
+  })
+}
+
+function asyncDouble(x, cb) {
+  setTimeout(() => cb(null, x * 2), 300);
+}
+function asyncTriple(x) {
+  return new Promise(r => {
+    setTimeout(() => r(x * 3), 300)
+  })
+}
+
+asyncMapCallback([1, 2, 3], asyncDouble, (err, res) => {
+  console.log("Callback:", res);
+})
+asyncMapPromise([1, 2, 3], asyncTriple)
+  .then(res => console.log("Promise:", res));
