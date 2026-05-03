e6ec9605d51abaf7f915ba6025c9995ba2003ed5
exmiz <daniel661234567890@gmail.com>
2026-05-03 12:27:15 +0300

    lab06.02

--- lab06/DataProcessing.js
+++ lab06/DataProcessing.js
@@ -12,3 +12,33 @@ ERROR Timeout occurred
 INFO Server stopped`;
   fs.writeFileSync(filePath, data);
 }
+
+async function processFile() {
+  const stream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+
+  const rl = readline.createInterface({
+    input: stream,
+    crlfDelay: Infinity
+  });
+
+  let totalLines = 0;
+  let errorLines = 0;
+
+  for await (const line of rl) {
+    totalLines++;
+
+    if (line.includes('ERROR')) {
+      errorLines++;
+    }
+  }
+
+  console.log('Result:');
+  console.log('Total rows:', totalLines);
+  console.log('ERROR rows:', errorLines);
+}
+
+async function main() {
+  createTestFile();
+  await processFile();
+}
+main();
